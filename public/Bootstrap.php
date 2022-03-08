c4851748d516cf7706c1363754a5fe6bc78e494e
Albertas Balzarevičius <albertas.balzarevicius@nfq.lt>
2022-03-08 19:01:12 +0200

    Added browser kit component, added simple example, create Client based on AbstractBrowser

---
+++ public/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+function echo_with_eol(string $string) {
+    echo $string . '</br>';
+}
---
+++ public/browser-kit/endpoint.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../Bootstrap.php';
+echo
+
+'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>My Page Title</title>
+    <meta name="description" content="My Page Description">
+</head>
+<body>
+<div class="example">hello</div>
+</body>
+</html>'
+;
